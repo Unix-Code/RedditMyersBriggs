1573c4359e6e438d0f5312c21d57bbff9fded335
Unix-Code <dmalakh@comcast.net>
2019-10-30 14:30:11 -0400

    change api url to elastic beanstalk

--- dist/js/services.js
+++ dist/js/services.js
@@ -3,7 +3,7 @@ angular.module('RedditMyersBriggs.services', [])
         var redditPersonaAPIService = {};
 
         redditPersonaAPIService.getPersonaResults = function (subreddit) {
-            return $http.get("http://127.0.0.1:5000/personas/stats/" + subreddit);
+            return $http.get("http://redditmyersbriggs-env.jq9336vacz.us-east-1.elasticbeanstalk.com/personas/stats/" + subreddit);
         }
 
         return redditPersonaAPIService;
