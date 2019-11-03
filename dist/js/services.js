2679c5c568c8d4fd28717dc7e7b195d133d72000
Unix-Code <dmalakh@comcast.net>
2019-11-03 14:24:19 -0500

    Update api url

--- dist/js/services.js
+++ dist/js/services.js
@@ -3,7 +3,7 @@ angular.module('RedditMyersBriggs.services', [])
         var redditPersonaAPIService = {};
 
         redditPersonaAPIService.getPersonaResults = function (subreddit) {
-            return $http.get("http://redditmyersbriggs-env.jq9336vacz.us-east-1.elasticbeanstalk.com/personas/stats/" + subreddit);
+            return $http.get("http://redditmyersbriggs-env.x97db3mfhy.us-east-1.elasticbeanstalk.com/personas/stats/" + subreddit);
         }
 
         return redditPersonaAPIService;
